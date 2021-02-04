19832a0a03bf27a356e7a616fa7f441a06ca0e9a
emiliojosecalvo <emiliojosecalvo@gmail.com>
2021-02-04 13:55:00 +0000

    another with img

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+
+// RainLetters Effect
 let mainCanv = document.getElementById("mycanvas");
 let canvtx = mainCanv.getContext("2d");
 let conHeight = document.querySelector('.header').clientHeight;
@@ -66,4 +69,34 @@ if (mainCanv.width > 576) {
 }
 
 setInterval(draw, 50);
+// End Rainletters effect
+
+//Animation
+(function () {
+    var elements;
+    var windowHeight;
+
+    function init() {
+        elements = document.querySelectorAll('.hidden');
+        windowHeight = window.innerHeight;
+    }
+
+    function checkPosition() {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var positionFromTop = elements[i].getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight <= 0) {
+                element.classList.add('fade-in-element');
+                element.classList.remove('hidden');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+
+    init();
+    checkPosition();
+})();
 
