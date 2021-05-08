24cbcc4a1a0d09ac9d7909389d4ee2b974cfc629
emiliojosecalvo <emiliojosecalvo@gmail.com>
2021-05-08 12:27:25 +0100

    updated WDB and CV

--- js/app.js
+++ js/app.js
@@ -1,76 +1,4 @@
 
-
-// RainLetters Effect
-let mainCanv = document.getElementById("mycanvas");
-let canvtx = mainCanv.getContext("2d");
-let conHeight = document.querySelector('.header').clientHeight;
-
-//making the canvas full screen
-// if (window.innerHeight > conHeight) {
-//     mainCanv.height = window.innerHeight;
-// } else {
-//     mainCanv.height = conHeight;
-// }
-mainCanv.height = conHeight;
-mainCanv.width = window.innerWidth;
-//letters characters - taken from the unicode charset
-let letters = "абвгдеж013579hijkl";
-//converting the string into an array of single characters
-letters = letters.split("");
-
-let font_size = 10;
-let columns = mainCanv.width / font_size; //number of columns for the rain
-// //an array of drops - one per column
-let drops = [];
-// //x below is the x coordinate
-// //1 = y co-ordinate of the drop(same for every drop initially)
-for (let x = 0; x < columns; x++)
-    drops[x] = 1;
-
-//Adjust the cascade to the size of the window
-function reportWindowSize() {
-    mainCanv.width = window.innerWidth;
-    mainCanv.height = conHeight;
-
-    columns = mainCanv.width / font_size;
-    drops = [];
-    for (let x = 0; x < columns; x++)
-        drops[x] = 1;
-}
-
-//drawing the characters
-function draw() {
-    //Black BG for the canvas
-    //translucent BG to show trail
-    canvtx.fillStyle = "rgba(0, 0, 0, 0.05)";
-    canvtx.fillRect(0, 0, mainCanv.width, mainCanv.height);
-
-    canvtx.fillStyle = "#0F0"; //green text
-    canvtx.font = font_size + "px arial";
-    //looping over drops
-    for (let i = 0; i < drops.length; i++) {
-        //a random letters character to print
-        let text = letters[Math.floor(Math.random() * letters.length)];
-        //x = i*font_size, y = value of drops[i]*font_size
-        canvtx.fillText(text, i * font_size, drops[i] * font_size);
-
-        //sending the drop back to the top randomly after it has crossed the screen
-        //adding a randomness to the reset to make the drops scattered on the Y axis
-        if (drops[i] * font_size > mainCanv.height && Math.random() > 0.975)
-            drops[i] = 0;
-
-        //incrementing Y coordinate
-        drops[i]++;
-    }
-}
-
-if (mainCanv.width > 576) {
-    window.addEventListener('resize', reportWindowSize);
-}
-
-setInterval(draw, 50);
-// End Rainletters effect
-
 //Animation
 (function () {
     var elements;
@@ -101,7 +29,7 @@ setInterval(draw, 50);
 })();
 
 // title
-var TxtType = function (el, toRotate, period) {
+const TxtType = function (el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
@@ -112,8 +40,8 @@ var TxtType = function (el, toRotate, period) {
 };
 
 TxtType.prototype.tick = function () {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+    const i = this.loopNum % this.toRotate.length;
+    const fullTxt = this.toRotate[i];
 
     if (this.isDeleting) {
         this.txt = fullTxt.substring(0, this.txt.length - 1);
@@ -144,7 +72,7 @@ TxtType.prototype.tick = function () {
 
 window.onload = function () {
     var elements = document.getElementsByClassName('typewrite');
-    for (var i = 0; i < elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
         var toRotate = elements[i].getAttribute('data-type');
         var period = elements[i].getAttribute('data-period');
         if (toRotate) {
